2679012e7340eb47cad5fad2f2cefcf98cf75209
Alex_M <iipo1004ek@gmail.com>
2021-04-26 16:27:43 +0300

    connect to mongo

---
+++ server/models/databases/GraphDb.js
@@ -0,0 +1,5 @@
+class GraphDb {
+
+};
+
+module.exports = GraphDb;
---
+++ server/routes/database.js
@@ -0,0 +1,14 @@
+const Router = require('express');
+const { cassandra, h2, graphDb, mongo, mysql, postgresql, redis } = require('./database/');
+
+const database = Router();
+
+database.use('/mysql', mysql);
+database.use('/postgresql', postgresql);
+database.use('/h2', h2);
+database.use('/mongodb', mongo);
+database.use('/redis', redis);
+database.use('/cassandra', cassandra);
+database.use('/graphdb', graphDb);
+
+module.exports = database
