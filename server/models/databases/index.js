aedae93e5b9045cc2d294c1f2966d4792c4b69d1
mzubkova <mary010118@gmail.com>
2021-05-12 00:44:48 +0300

    update connect Postgresql

--- server/models/databases/index.js
+++ server/models/databases/index.js
@@ -1,16 +1,22 @@
-const Cassandra = require('./Cassandra');
-const GraphDb = require('./GraphDb');
-const H2 = require('./H2');
+// const Cassandra = require('./Cassandra');
+// const GraphDb = require('./GraphDb');
+// const H2 = require('./H2');
 const { Mongo, PersonSchema } = require('./Mongo');
-const MySql = require('./MySql');
+// const MySql = require('./MySql');
 const PostgreSql = require('./PostgreSql');
 
+// module.exports = {
+//   Cassandra,
+//   GraphDb,
+//   H2,
+//   Mongo,
+//   MySql,
+//   PostgreSql,
+//   PersonSchema
+// };
+
 module.exports = {
-  Cassandra,
-  GraphDb,
-  H2,
   Mongo,
-  MySql,
   PostgreSql,
-  PersonSchema
+
 };
