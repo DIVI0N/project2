ae58416124d67e5d393df32ecdfbfc7222efe30e
DIVI0N <80914142+DIVI0N@users.noreply.github.com>
2021-05-14 13:53:07 +0300

    addMySQL

--- server/models/databases/index.js
+++ server/models/databases/index.js
@@ -2,7 +2,7 @@
 // const GraphDb = require('./GraphDb');
 // const H2 = require('./H2');
 const { Mongo, PersonSchema } = require('./Mongo');
-// const MySql = require('./MySql');
+const MySql = require('./MySql');
 const PostgreSql = require('./PostgreSql');
 
 module.exports = {
@@ -10,7 +10,7 @@ module.exports = {
   // GraphDb,
   // H2,
   Mongo,
-  // MySql,
+  MySql,
   PostgreSql,
   PersonSchema
 };
