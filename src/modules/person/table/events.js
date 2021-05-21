806b7a413cf045e3bdb5ea0deab27a7c5740098f
Alex_M <iipo1004ek@gmail.com>
2021-05-22 02:34:10 +0300

    start update person

--- src/modules/person/table/events.js
+++ src/modules/person/table/events.js
@@ -1,11 +1,27 @@
-import { getData, getPerson, support, url } from '../..';
-
+import { getData, getPerson, support, url, getFetch } from '../..';
+const { lsGet } = support;
 export const deleteRow = (e) => {
-  const { lsGet } = support;
+
   if (e.target.getAttribute('id') === 'deleteRow') {
     const id = e.target.getAttribute('data-id');
     const delUrl = `${url.database}/${lsGet('db')}?id=${id}`;
     getData(delUrl, 'DELETE');
     getPerson();
+  }
+};
+
+export const changeRowData = (e) => {
+  if (e.target.classList.contains('table__row-item')) {
+    e.target.setAttribute('contenteditable', 'true');
+  }
+};
+
+export const blurRow = (e) => {
+  if (e.target.classList.contains('table__row-item')) {
+    e.target.setAttribute('contenteditable', 'false');
+    const blurUrl = `${url.database}/${lsGet('db')}`;
+    const dataName = e.target.getAttribute('data-name');
+    console.log(dataName);
+    // getFetch(blurUrl);
   }
 };
--- src/modules/person/table/tableEvent.js
+++ src/modules/person/table/tableEvent.js
@@ -1,10 +1,13 @@
-import { deleteRow } from './events';
+import { deleteRow, changeRowData, blurRow } from './events';
 
 export default function tableEvent() {
   const table = document.querySelector('#mainTable');
 
-  table.addEventListener('click', (e) => {
-    deleteRow(e);
+  table.addEventListener('click', deleteRow);
+  table.addEventListener('dblclick', () => {
+    changeRowData(e);
+    e.target.addEventListener('blur', blurRow);
   });
+
 }
 
