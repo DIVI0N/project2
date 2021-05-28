bd1e57d518b6e4a42f0bc7bddf6b02abc56385b0
DIVI0N <80914142+DIVI0N@users.noreply.github.com>
2021-05-28 11:23:09 +0300

    +connect

---
+++ server/models/connect/postgres.js
@@ -0,0 +1,19 @@
+const marina = {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'FaJ_761FA',
+    database: 'postgresql_persons',
+    port: 5432,
+    ssl: false,
+};
+
+const dima = {
+    host: 'localhost',
+    user: 'postgres',
+    password: 'root',
+    database: 'postgres',
+    port: 5432,
+    ssl: false,
+};
+
+module.exports = { marina, dima };
