00094770a07341fcb9c7181c33f406cfed97b711
Alex_M <iipo1004ek@gmail.com>
2021-05-30 22:50:43 +0300

    add success setting message

--- server/routes/database/redis.js
+++ server/routes/database/redis.js
@@ -1,18 +1,18 @@
 const { Router } = require('express');
 const redis = Router();
 const { Redis } = require('../../models');
-const { support } = require('../../support');
+const { support, settingFields } = require('../../support');
 const { authToken } = support;
 
 redis.get('/', authToken, (req, res, next) => {
   Redis.getRequest(req, res);
 });
 
-redis.post('/', authToken, (req, res, next) => {
+redis.post('/', authToken, settingFields, (req, res, next) => {
   Redis.create(req, res);
 });
 
-redis.put('/', authToken, (req, res, next) => {
+redis.put('/', authToken, settingFields, (req, res, next) => {
   Redis.update(req, res);
 });
 
